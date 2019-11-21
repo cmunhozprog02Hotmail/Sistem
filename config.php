c712e35d0102ba1b23a54f11004cc20190f28c1c
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2019-11-21 11:13:15 -0300

    Segundo

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+const LBS = 'Library/';
+const VIEWS = 'Views/';
+define('RQ', 'Resource/');
+define('URL', 'http://localhost/pdhn/Sistem/');
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require "config.php";
+
+$url = isset($_GET["url"]) ? $_GET["url"]:"Index/index";
+$url = explode("/", $url);
+$controller = "";
+$method = "";
+if(isset($url[0])){
+    $controller = $url[0];
+}
+if(isset($url[1])){
+    if(($url[1] != '')){
+        $method = $url[1];
+    }
+}
+//Chama as constatantes do arquivo config.php
+spl_autoload_register(function($class){
+
+});
+
+ echo $controller." ----- ".$method;
+
+
+?>
